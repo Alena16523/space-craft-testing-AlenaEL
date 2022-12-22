9ce78429dee65c4d4e72c21dcd60d3b4cf421544
Alena <alenaelhajassaad@yahoo.com>
2022-12-21 20:51:31 -0500

    integrational testing

--- src/test/java/com/cydeo/spacecraft/integration/unit/service/CreateTargetServiceImplTest.java
+++ src/test/java/com/cydeo/spacecraft/integration/unit/service/CreateTargetServiceImplTest.java
@@ -1,3 +1,5 @@
+package com.cydeo.spacecraft.integration.unit.service;
+
 import com.cydeo.spacecraft.entity.Target;
 import com.cydeo.spacecraft.enumtype.Level;
 import com.cydeo.spacecraft.service.impl.CreateTargetServiceImpl;
---
+++ target/test-classes/sql/remove_data.sql
@@ -0,0 +1,5 @@
+
+
+DELETE FROM player where id > 0
+DELETE FROM target where id > 0
+DELETE FROM game where id > 0
